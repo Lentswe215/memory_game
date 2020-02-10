6913e5be7ac07a64689abcc95876e956954596d8
Lentswe215 <ephraimmamonong@gmail.com>
2020-02-10 14:11:00 +0200

    finished

---
+++ scripts.js
@@ -0,0 +1,59 @@
+let clickedCard = null;
+let preventClick = false;
+let count = 0;
+
+let colorsOfCards = [
+    "red",
+    "blue",
+    "pink",
+    "yellow",
+    "purple",
+    "green",
+    "aqua",
+    "orange"
+];
+
+let cards = [...document.querySelectorAll(".card")];
+
+for (let color of colorsOfCards) {
+    let cardAIndex = parseInt(Math.random() * cards.length);
+    let cardA = cards[cardAIndex];
+    cards.splice(cardAIndex, 1);
+    cardA.className += ` ${color}`;
+    cardA.setAttribute("card-color", color);
+
+    let cardBIndex = parseInt(Math.random() * cards.length);
+    let cardB = cards[cardBIndex];
+    cards.splice(cardBIndex, 1);
+    cardB.className += ` ${color}`;
+    cardB.setAttribute("card-color", color);
+}
+function onCardClicked(e) {
+    let currentCard = e.currentTarget;
+
+    if (preventClick || currentCard === clickedCard || target.className.includes("flipped"))
+    }
+    currentCard.className = currentCard.className.replace("frontCard-hidden", "").trim();
+
+    currentCard.className += " flipped";
+
+    if (!clickedCard) {
+        clickedCard = currentCard;
+    } else if (clickedCard) {
+        if (clickedCard.getAttribute("card-color") !== currentCard.getAttribute("card-color")) {
+            preventClick = true;
+            setTimeout(() => {
+                clickedCard.className = clickedCard.className.replace("flipped", "").trim() + " frontCard-hidden";
+                currentCard.className = currentCard.className.replace("flipped", "").trim() + " frontCard-hidden";
+                clickedCard = null;
+                preventClick = false;
+            }, 1000);
+        } else {
+            clickedCard = null;
+            count++;
+        }
+    }
+    if (count == 8) {
+        alert("Congrats You Won!!!");
+    }
+}
